2c65965e16563f8631d5cbdd0f588a30685dd9e6
rodrigoVelas <124946661+rodrigoVelas@users.noreply.github.com>
2025-07-15 23:57:52 -0600

    Update app.js
    cambios en la ruta

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   const menuContenedor = document.getElementById('menu');
 
   try {
-    const response = await fetch('https://menu-japones-api.onrender.com/menu');
+    const response = await fetch("https://menu-japones-api.onrender.com/api/menu");
     const data = await response.json();
 
     if (!data || !data.length) {
